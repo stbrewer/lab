f0f5a640851a1334d356b9fa9f265219836cad52
Steve Brewer <brewer.stephen.c@gmail.com>
2025-03-08 09:38:28 -0600

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,8 +1,6 @@
-# s3.tf
 resource "aws_s3_bucket" "backups" {
   bucket        = "${var.resource_prefix}-backups"
-  acl           = "public-read"  # Publicly readable backups
-  force_destroy = true           # Allows bucket to be destroyed with objects inside
+  force_destroy = true           # Allows the bucket to be deleted even if it has objects
 
   website {
     index_document = "index.html"
@@ -13,20 +11,22 @@ resource "aws_s3_bucket" "backups" {
   }
 }
 
-# Bucket policy to allow public listing and read access.
 resource "aws_s3_bucket_policy" "backups_policy" {
   bucket = aws_s3_bucket.backups.id
   policy = jsonencode({
     Version   = "2012-10-17",
-    Statement = [{
-      Sid       = "PublicReadGetObject",
-      Effect    = "Allow",
-      Principal = "*",
-      Action    = ["s3:GetObject", "s3:ListBucket"],
-      Resource  = [
-        aws_s3_bucket.backups.arn,
-        "${aws_s3_bucket.backups.arn}/*"
-      ]
-    }]
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = ["s3:GetObject", "s3:ListBucket"],
+        Resource  = [
+          aws_s3_bucket.backups.arn,
+          "${aws_s3_bucket.backups.arn}/*"
+        ]
+      }
+    ]
   })
 }
+
