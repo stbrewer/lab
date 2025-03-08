33a3e2be56bb617d143a785080b93233fc65f318
Steve Brewer <brewer.stephen.c@gmail.com>
2025-03-08 12:11:13 -0600

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,23 +1,23 @@
 # eks.tf
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "20.8.4"
+  version         = "20.8.4"        # Fully compatible with AWS provider 5.x
   cluster_name    = "${var.resource_prefix}-eks"
   cluster_version = "1.25"
 
-  vpc_id     = aws_vpc.main.id
-  subnet_ids = [aws_subnet.private.id, aws_subnet.private2.id]
+  vpc_id          = aws_vpc.main.id
+  subnet_ids      = [aws_subnet.private.id, aws_subnet.private2.id]
 
   cluster_enabled_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
 
-  eks_managed_node_groups = {
+  eks_managed_node_groups = {       # Ensure this exact naming (no other node groups!)
     eks_nodes = {
-      desired_size = 1
+      desired_size   = 1
       max_size       = 1
       min_size       = 1
 
       instance_types = ["t3.medium"]
-      key_name      = "your-key-pair"  # Replace with your actual key pair name
+      key_name       = "your-key-pair"  # Replace this!
 
       tags = {
         Name = "${var.resource_prefix}-eks-node"
