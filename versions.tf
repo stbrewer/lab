a96beee1a8ab37d4355fd74bfa1c6f4b6baa464f
Steve Brewer <brewer.stephen.c@gmail.com>
2025-03-08 09:57:13 -0600

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,5 +1,14 @@
 terraform {
   required_version = ">= 1.4.0"
+
+  backend "s3" {
+    bucket         = "my-terraform-state-894370042961"   # The S3 bucket name you created
+    key            = "terraform/state.tfstate"           # The path within the bucket for your state file
+    region         = "us-west-2"                         # Your AWS region
+    dynamodb_table = "terraform-locks-894370042961"        # The DynamoDB table name for state locking
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
