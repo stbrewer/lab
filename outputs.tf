c3afe6495b73ff3ded808611f71263fc0e82bf06
Steve Brewer <brewer.stephen.c@gmail.com>
2025-03-08 12:32:19 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -13,8 +13,9 @@ output "eks_cluster_name" {
   description = "Name of the EKS cluster"
   value       = module.eks.cluster_id
 }
-
+/*
 output "load_balancer_url" {
   description = "URL to access the containerized web application"
   value       = kubernetes_service.tasky_lb.status[0].load_balancer[0].ingress[0].hostname
 }
+*/
