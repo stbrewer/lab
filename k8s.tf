b3ae7b9023019b92f7413146d52b48a79ab58037
Steve Brewer <brewer.stephen.c@gmail.com>
2025-03-08 13:01:03 -0600

    Update k8s.tf

--- k8s.tf
+++ k8s.tf
@@ -3,7 +3,7 @@
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.eks.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
+  token                  = module.eks.cluster_token
 }
 
 
