1ab64f6efb6a7fe949c4840c8b3d43ef16213355
Steve Brewer <brewer.stephen.c@gmail.com>
2025-03-08 19:39:04 -0600

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -48,7 +48,7 @@ resource "aws_iam_role" "eks_cluster_role" {
     }]
   })
 }
-/*
+
 resource "kubernetes_config_map" "aws_auth" {
   metadata {
     name      = "aws-auth"
@@ -72,7 +72,7 @@ resource "kubernetes_config_map" "aws_auth" {
     YAML
   }
 }
-*/
+
 resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.eks_cluster_role.name
