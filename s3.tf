1191a38cd9e8299d167323221765723349c0a8d1
Steve Brewer <brewer.stephen.c@gmail.com>
2025-03-08 18:38:14 -0600

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -3,7 +3,7 @@ data "aws_s3_bucket" "backups" {
 }
 /*
 resource "aws_s3_bucket" "backups" {
-  bucket = data.aws_s3_bucket.backups.bucket
+  bucket = data.data.aws_s3_bucket.backups.bucket
   #acl           = "public-read"  # (if you need public-read; otherwise consider using a more restrictive ACL)
   force_destroy = true           # Allows the bucket to be deleted even if it has objects
 
@@ -21,7 +21,7 @@ resource "aws_s3_bucket" "backups" {
 }
 */
 resource "aws_s3_bucket_ownership_controls" "backups_ownership" {
-  bucket = aws_s3_bucket.backups.id
+  bucket = data.aws_s3_bucket.backups.id
 
   rule {
     object_ownership = "BucketOwnerEnforced"
@@ -29,7 +29,7 @@ resource "aws_s3_bucket_ownership_controls" "backups_ownership" {
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "backups" {
-  bucket = aws_s3_bucket.backups.id
+  bucket = data.aws_s3_bucket.backups.id
   rule {
     id     = "delete-old-backups"
     status = "Enabled"
@@ -41,7 +41,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "backups" {
 
 # Disable block public access for this bucket so that our policy can be applied
 resource "aws_s3_bucket_public_access_block" "backups" {
-  bucket                  = aws_s3_bucket.backups.id
+  bucket                  = data.aws_s3_bucket.backups.id
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
@@ -49,7 +49,7 @@ resource "aws_s3_bucket_public_access_block" "backups" {
 }
 
 resource "aws_s3_bucket_policy" "backups_policy" {
-  bucket = aws_s3_bucket.backups.id
+  bucket = data.aws_s3_bucket.backups.id
   policy = jsonencode({
     Version   = "2012-10-17",
     Statement = [
@@ -59,8 +59,8 @@ resource "aws_s3_bucket_policy" "backups_policy" {
         Principal = "*",
         Action    = ["s3:GetObject", "s3:ListBucket"],
         Resource  = [
-          aws_s3_bucket.backups.arn,
-          "${aws_s3_bucket.backups.arn}/*"
+          data.aws_s3_bucket.backups.arn,
+          "${data.aws_s3_bucket.backups.arn}/*"
         ]
       }
     ]
