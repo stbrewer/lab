72174130ccd6630e55fd659c80fffa99760db86b
Steve Brewer <brewer.stephen.c@gmail.com>
2025-03-08 19:07:53 -0600

    Update k8s.tf

--- k8s.tf
+++ k8s.tf
@@ -1,11 +1,11 @@
 # k8s.tf
-/*
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.eks.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.eks.token
 }
-
+/*
 # All Kubernetes resources must explicitly depend on these data sources:
 
 resource "kubernetes_service_account" "tasky_sa" {
