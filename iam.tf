45dc9a95139db28f5b47dc6e0721ba787f83c3dd
Steve Brewer <brewer.stephen.c@gmail.com>
2025-03-08 20:27:47 -0600

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -29,3 +29,35 @@ resource "aws_iam_instance_profile" "db_instance_profile" {
   name = "${var.resource_prefix}-db-instance-profile"
   role = aws_iam_role.db_instance_role.name
 }
+
+resource "aws_iam_role" "eks_admin_role" {
+  name = "eks-admin-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::894370042961:user/eks-admin"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_admin_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_admin_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_admin_AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks_admin_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_admin_AmazonVPCFullAccess" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
+  role       = aws_iam_role.eks_admin_role.name
+}
