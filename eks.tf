fc67d782e263ddfe6dd0f020dcdbcee4f4e8ca5b
Steve Brewer <brewer.stephen.c@gmail.com>
2025-03-08 20:28:05 -0600

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -12,7 +12,8 @@ module "eks" {
   cluster_enabled_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
 
   # 🛡️ IAM Role for EKS Cluster
-  iam_role_arn = aws_iam_role.eks_cluster_role.arn
+  iam_role_arn = aws_iam_role.eks_admin_role.arn
+
 
   eks_managed_node_groups = var.deploy_node_group ? {
     eks_nodes = {
